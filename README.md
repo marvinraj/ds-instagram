1ac1e3c2da6d1c674aeefb891eecbcb1534a29d6
Marvin <marvinraj77@gmail.com>
2025-01-02 15:05:25 +0800

    connected to mysql database

--- README.md
+++ README.md
@@ -5,3 +5,17 @@ a full-stack application that works similarly to instagram but with minimal feat
 1. front-end --> reactjs
 2. backend --> nodejs, expressjs
 3. database --> mysql
+
+## users:
+1. user 1
+2. user 2
+
+## features:
+1. upload an image
+2. like an image
+3. comment on an image
+
+## pages:
+1. landing page
+2. login page
+3. feed page
--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
 const express = require('express');
+const connectDB = require('./config/db');
+
+require('dotenv').config(); // loads the env variables
 
 const app = express()
-const port = 5000;
+const port = process.env.PORT || 5000; // store port from env variable
 
 app.get('/', (req, res) => {
     res.send("Hello world");
 });
 
+// db connection
+connectDB.connect((err) => {
+    if (err){
+        console.log("error connecting to mysql");
+    } else {
+        console.log("successful connection with mysql!");
+    }
+})
+
 app.listen(port, () => {
-    console.log("server started at http://localhost:" + port);
+    console.log("server running at http://localhost:" + port);
 });
